bab785241ef4403b09c950b986ef28c3831b50f1
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-07-23 17:08:28 +0200

    PEP8

--- myqueue/slurm.py
+++ myqueue/slurm.py
@@ -48,7 +48,8 @@ class SLURM(Queue):
             '#!/bin/bash -l\n'
             'id=$SLURM_JOB_ID\n'
             'mq={home}/slurm-$id\n'
-            '(touch $mq-0 && cd {dir} && {cmd} && touch $mq-1) || touch $mq-2\n'
+            '(touch $mq-0 && cd {dir} && {cmd} && touch $mq-1) || '
+            'touch $mq-2\n'
             .format(home=home, dir=task.folder, cmd=cmd))
 
         p = subprocess.Popen(sbatch,
