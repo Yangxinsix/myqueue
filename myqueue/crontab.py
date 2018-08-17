95a07a1baffce136dbef5e466ae68c645d5d4cf7
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2018-08-17 09:07:05 +0200

    improved crontab

--- myqueue/crontab.py
+++ myqueue/crontab.py
@@ -4,9 +4,9 @@ import subprocess
 def install_crontab_job():
     p = subprocess.run(['crontab', '-l'], stdout=subprocess.PIPE)
     crontab = p.stdout
-    cmd = b'python3 -m myqueue kick'
+    cmd = b'bash -lc "python3 -m myqueue kick >> ~/.myqueue/kick.log"'
     if cmd in crontab:
         raise ValueError('Already installed!')
-    crontab += b'\n0,10,20,30,40,50 * * * * ' + cmd + b'\n'
+    crontab += b'\n0,30 * * * * ' + cmd + b'\n'
     p = subprocess.Popen(['crontab', '-'], stdin=subprocess.PIPE)
     p.communicate(crontab)
