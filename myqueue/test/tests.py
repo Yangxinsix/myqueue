c4312b8e04350b7f66f87c0df7e1b672b75533a3
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2019-04-23 11:37:46 +0200

    Typo

--- myqueue/test/tests.py
+++ myqueue/test/tests.py
@@ -193,7 +193,7 @@ def workflow():
     mq('submit sleep+3@1:1m -w')
     time.sleep(2)
     Path('wf.py').write_text(wf)
-    mq('workflow wf.py . -t echo+hello')
+    mq('workflow wf.py . -t touch+hello')
     wait()
     assert states() == 'dd'
 
