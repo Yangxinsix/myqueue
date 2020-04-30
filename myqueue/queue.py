335c84d36fddd50eb4722d70a33ecacf078e02af
Jens Jørgen Mortensen <jj@smoerhul.dk>
2020-04-30 10:40:45 +0200

    Fix sync command

--- myqueue/queue.py
+++ myqueue/queue.py
@@ -321,12 +321,23 @@ class Queue(Lock):
         self._read()
         in_the_queue = {'running', 'hold', 'queued'}
         ids = self.scheduler.get_ids()
+        cancel = []
         remove = []
         for task in self.tasks:
-            if task.state in in_the_queue and task.id not in ids:
-                remove.append(task)
-            elif not task.folder.is_dir():
-                remove.append(task)
+            if task.id not in ids:
+                if task.state in in_the_queue:
+                    cancel.append(task)
+                if not task.folder.is_dir():
+                    remove.append(task)
+
+        if cancel:
+            if self.dry_run:
+                print(plural(len(cancel), 'job'), 'to be canceled')
+            else:
+                for task in cancel:
+                    task.state = 'CANCELED'
+                    self.changed.add(task)
+                print(plural(len(cancel), 'job'), 'canceled')
 
         if remove:
             if self.dry_run:
