0fc62f45fea3beff36cc62bd64efb68a7487b98f
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-05-04 15:00:42 +0200

    strip

--- myqueue/slurm.py
+++ myqueue/slurm.py
@@ -149,6 +149,7 @@ class SLURM(Scheduler):
         p = subprocess.run(cmd, stdout=subprocess.PIPE)
         mem = 0
         for line in p.stdout.splitlines():
+            line = line.strip()
             if line.endswith(b'K'):
                 assert mem == 0
                 mem = int(line[:-1]) * 1000
