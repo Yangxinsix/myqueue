fbfa4201fb88abf576fd3d6999ab9cbbb07fe7e5
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2020-11-19 08:47:52 +0100

    Create bugfix version 20.11.1

--- setup.py
+++ setup.py
@@ -26,4 +26,5 @@ setup(name='myqueue',
           'Programming Language :: Python :: 3 :: Only',
           'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
           'Topic :: Scientific/Engineering'])
