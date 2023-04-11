002c93320c379405f12aa3369ba46263416fa5b7
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-04-11 13:24:27 +0200

    Version 23.4

--- myqueue/__init__.py
+++ myqueue/__init__.py
@@ -9,7 +9,7 @@ if TYPE_CHECKING:
     from myqueue.task import Task
 
 
-__version__ = '23.1.0'
+__version__ = '23.4.0'
 
 
 def submit(*tasks: Task, verbosity: int = 1, dry_run: bool = False) -> None:
