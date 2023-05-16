b9bbb5f55f0a14a9d53e75b190709baaa1c2d5ff
Jens Jørgen Mortensen <jensj@fysik.dtu.dk>
2023-05-16 17:17:51 +0200

    Finish test

--- myqueue/test/error_msg_test.py
+++ myqueue/test/error_msg_test.py
@@ -1,16 +1,19 @@
 from myqueue.errors import parse_stderr
-from pathlib import Path
+
 txt = """
-rank=02 L00: Traceback (most recent call last):
+L00: Traceback (most recent call last):
 ...
-rank=02 L17: ModuleNotFoundError: No module named 'gpaw.bz_tools'
+L17: ModuleNotFoundError: No module named 'gpaw.bz_tools'
 e>
  at line 2193
-[x069.nifl.fysik.dtu.dk:08806] PMIX ERROR: UNREACHABLE in file server/pmix_server.c at line 2193
-[x069.nifl.fysik.dtu.dk:08806] PMIX ERROR: UNREACHABLE in file server/pmix_server.c at line 2193
-[x069.nifl.fysik.dtu.dk:08806] PMIX ERROR: UNREACHABLE in file server/pmix_server.c at line 2193
-[x069.nifl.fysik.dtu.dk:08806] 47 more processes have sent help message help-mpi-api.txt / mpi-abort
-[x069.nifl.fysik.dtu.dk:08806] Set MCA parameter "orte_base_help_aggregate" to 0 to see all help / error messages
+[x069] PMIX ERROR: UNREACHABLE in file server/pmix_server.c at line 2193
+[x069] PMIX ERROR: UNREACHABLE in file server/pmix_server.c at line 2193
+[x069] 47 more processes have sent help message help-mpi-api.txt / mpi-abort
+[x069] Set MCA ... "orte_base_help_aggregate" to 0 to see all help / error msg
 """
-a, b = parse_stderr(txt)
-print(a, b)
+
+
+def test_niflheim_error():
+    err, oom = parse_stderr(txt)
+    assert not oom
+    assert err == "L17: ModuleNotFoundError: No module named 'gpaw.bz_tools'"
